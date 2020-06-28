c9457125af3d4a4e834ee5fcebee0890cb99e832
landon tipantiza <landontipantiza@gmail.com>
2020-06-28 03:39:45 +0000

    finsihed databse and seeds and added routes to access db for users and maps"

--- routes/maps.js
+++ routes/maps.js
@@ -10,12 +10,11 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
+    let query = `SELECT * FROM maps;`;
     db.query(query)
       .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+        const maps = data.rows;
+        res.json({ maps });
       })
       .catch(err => {
         res
