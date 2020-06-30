b84d154842b786a16bb68fe4eaf8099f593d25b6
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-30 18:58:31 +0000

    Added favs routes for GET and POST

--- routes/favs.js
+++ routes/favs.js
@@ -1,6 +1,12 @@
+const express = require('express');
+const router  = express.Router();
+
+
 module.exports = (db) => {
+
+  //GET from /api/favs
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM favorites;`;
+    let query = `SELECT count(*) FROM favorites WHERE map_id = 1;`;
     db.query(query)
       .then(data => {
         const favs = data.rows;
@@ -11,12 +17,23 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
-
   });
-
-
-
-
-
-  
+  //POST from /api/favs
+  router.post("/post", (req, res) => {
+    const user = req.session.user_id;
+    const inputs = [user, req.body.mapID];
+    let query = `INSERT INTO favorites (user_id, map_id) VALUES ($1, $2);`;
+    db.query(query, inputs)
+      .then(data => {
+        const favs = data.rows;
+        res.json({ favs });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
 }
+
