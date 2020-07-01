2f3460f950c96ff6cb4575d498c2e6afc8366ad4
cjfelice <monocularmedia@gmail.com>
2020-07-01 17:42:21 +0000

    small map fix

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -367,7 +367,6 @@ $(`#create-map`).on('click', function() {
   $(document).ready(function() {
     var autocomplete;
     autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
-      types: ['geocode'],
     });
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
       var near_place = autocomplete.getPlace();
